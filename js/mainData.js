8fbd727c82814b06f504d66d90cba27e3efa6645
olimpiuus <kolegin.yuriu@gmail.com>
2022-09-25 15:15:09 -0600

    finish day 3

--- js/mainData.js
+++ js/mainData.js
@@ -1,5 +1,34 @@
 const mainData = (() => {
+
+
+    const renderTopViews = (arr) => {
+            const topViewsContainer = document.querySelector('.filter__gallery')
+            topViewsContainer.innerHTML = ''
+            arr.forEach(element => {
+                topViewsContainer.insertAdjacentHTML("beforeend", `
+                <div class="product__sidebar__view__item set-bg" data-setbg="${element.image}";">
+                    <div class="ep">${element.rating} / 10</div>
+                    <div class="view"><i class="fa fa-eye"></i> ${element.views}</div>
+                    <h5><a href="/anime-details.html">${element.title}</a></h5>
+                </div>
+             `)
+            })
+
+            topViewsContainer.querySelectorAll('.set-bg').forEach((el) => {
+                console.log(el);
+                const src = el.dataset.setbg
+                el.style.backgroundImage = `url(${src})`
+            })
+        }
+        // const topViewsContainer = document.querySelector('.filter__gallery')
+
+
+
+
     fetch('https://glo-intensive-sept-23-2022-default-rtdb.firebaseio.com/anime.json')
         .then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => renderTopViews(data.sort((a, b) => b.views - a.views).slice(0, 5)))
+
+
+
 })()
